76af25c93d6b5a699851d6a16d82ec9cb2f5c134
Shreyash-BMSCE <53753410+Shreyash-BMSCE@users.noreply.github.com>
2020-09-27 04:39:58 +0530

    first commit

---
+++ Unity2D/Number_Wizard_Console/Number Wizard Console/Assets/NumberWizard.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.Win32.SafeHandles;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NumberWizard : MonoBehaviour
+{
+    int max = 1000;
+    int min = 1;
+    int guess = 500;
+    // Start is called before the first frame update
+    void Start() 
+    {
+        
+        Debug.Log("Welcome to Number Wizard, Yo");
+        Debug.Log("Pick a Number, shhh don't tell me anything...");
+        Debug.Log("Highest number to pick from is " + max);
+        Debug.Log("Lowest number to pick from is " + min);
+        Debug.Log("Tell me if your number is Higher or Lower than 500");
+        Debug.Log("Push Up = Higher, Push Down = Lower, Correct = Enter");
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            Debug.Log("Up arrow key was pressed.");
+            min = guess;
+            Debug.Log(guess);
+        }
+        else if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            Debug.Log("Down arrow key was pressed.");
+            max = guess;
+            Debug.Log(guess);
+        }
+        else if (Input.GetKeyDown(KeyCode.Return))
+        {
+            Debug.Log("Enter key was pressed.");
+        }
+    }
+}
---
+++ Unity2D/Number_Wizard_Console_2019/Number Wizard Console 2019/Assets/NumberWiz2019.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NumberWiz2019 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    int max = 1000;
+    int min = 1;
+    int guess = 500;
+    void Start()
+    {
+        Debug.Log("I welcometh thee to the w'rld of gaming");
+        Debug.Log("Enter your lucky number ...shh don't tell it to me");
+        Debug.Log("The maximum limit you can choose from is " + max);
+        Debug.Log("The minimum limit you can choose from is " + min);
+        Debug.Log("Tell me if your number is higher or lower than " + guess);
+        Debug.Log("Higher = Push Up Arrow key, Lower = Push Down Arrow Key, Correct = Push Enter");
+        max = max + 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            min = guess;
+            guess = (max + min) / 2;
+            Debug.Log("Is it higher or lower than... " + guess);
+        }
+        else if(Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            max = guess;
+            guess = (max + min) / 2;
+            Debug.Log("Is it higher or lower than... " + guess);
+        }
+        else if(Input.GetKeyDown(KeyCode.Return))
+        {
+            Debug.Log("Talk about EAZY.");
+        }
+    }
+}
